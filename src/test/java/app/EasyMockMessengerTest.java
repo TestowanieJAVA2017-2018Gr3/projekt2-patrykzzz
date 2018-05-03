e3b6dc06263ce4601454b490808ea3b99d74e3a8
Patryk Miłek <patrykmilek96@gmail.com>
2018-05-03 15:14:42 +0200

    Finish EasyMock tests

--- src/test/java/app/EasyMockMessengerTest.java
+++ src/test/java/app/EasyMockMessengerTest.java
@@ -1,7 +1,9 @@
 package app;
 
 import messenger.ConnectionStatus;
+import messenger.MalformedRecipientException;
 import messenger.MessageService;
+import messenger.SendingStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +18,7 @@ public class EasyMockMessengerTest
     @BeforeEach
     void SetUp()
     {
-        messageService = createMock(MessageService.class);
+        messageService = createNiceMock(MessageService.class);
         _target = new Messenger(messageService);
     }
 
@@ -32,5 +34,77 @@ public class EasyMockMessengerTest
 
         //Assert
         assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void TestConnection_ForEverythingOk_ShouldServiceMethodBeCalledOnce()
+    {
+        //Arrange
+        expect(messageService.CheckConnection(isA(String.class)))
+                .andReturn(ConnectionStatus.SUCCESS)
+                .times(1);
+        replay(messageService);
+
+        //Act
+        _target.TestConnection("test");
+
+        //Assert
+        verify(messageService);
+    }
+
+    @Test
+    void TestConnection_ForWrongServerName_ShouldReturnOne()
+    {
+        //Arrange
+        expect(messageService.CheckConnection(anyString())).andReturn(ConnectionStatus.FAILURE);
+        replay(messageService);
+
+        //Act
+        var result = _target.TestConnection(anyString());
+
+        //Assert
+        assertThat(result).isOne();
+    }
+
+    @Test
+    void SendMessage_ForEverythingOk_ShouldReturnZero() throws MalformedRecipientException
+    {
+        //Arrange
+        expect(messageService.Send("test", "message")).andStubReturn(SendingStatus.SENT);
+        replay(messageService);
+
+        //Act
+        var result = _target.SendMessage("test", "message");
+
+        //Assert
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void SendMessage_ForNullServer_ShouldNotThrowException() throws MalformedRecipientException
+    {
+        //Arrange
+        expect(messageService.Send(null, "Test")).andThrow(new MalformedRecipientException());
+        replay(messageService);
+
+        //Act
+        var result = _target.SendMessage(null, "Test");
+
+        //Assert
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void SendMessage_ForSomethingWrong_ShouldReturnOne() throws MalformedRecipientException
+    {
+        //Arrange
+        expect(messageService.Send(anyString(), anyString())).andReturn(SendingStatus.SENDING_ERROR).once();
+        replay(messageService);
+
+        //Act
+        var result = _target.SendMessage("test", "Test");
+
+        //Assert
+        assertThat(result).isOne();
     }
 }
