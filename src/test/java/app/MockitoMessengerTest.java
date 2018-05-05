0065138c712eb7c2c6d7609ff50e0151996b0d25
Patryk Miłek <patrykmilek96@gmail.com>
2018-05-05 15:19:32 +0200

    Finish Mockito tests

--- src/test/java/app/MockitoMessengerTest.java
+++ src/test/java/app/MockitoMessengerTest.java
@@ -1,11 +1,14 @@
 package app;
 
 import messenger.ConnectionStatus;
+import messenger.MalformedRecipientException;
 import messenger.MessageService;
-import org.assertj.core.api.Assertions;
+import messenger.SendingStatus;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 public class MockitoMessengerTest
@@ -31,6 +34,71 @@ public class MockitoMessengerTest
 
         //Assert
         verify(_messageService).CheckConnection("test");
-        Assertions.assertThat(result).isEqualTo(0);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void SendMessage_ForWrongMessage_ShouldInvocationSucceed() throws MalformedRecipientException
+    {
+        //Arrange
+        when(_messageService.Send("test", null)).thenThrow(MalformedRecipientException.class);
+
+        //Act
+        var result = _target.SendMessage("test", null);
+
+        //Assert
+        verify(_messageService).Send("test", null);
+    }
+
+    @Test
+    void SendMessage_ForNullServer_ShouldReturnTwo() throws MalformedRecipientException
+    {
+        //Arrange
+        when(_messageService.Send(null, "any")).thenThrow(MalformedRecipientException.class);
+
+        //Act
+        var result = _target.SendMessage(null, "any");
+
+        //Assert
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void SendMessage_ForInternalServiceError_ShouldReturnTwo() throws MalformedRecipientException
+    {
+        //Arrange
+        when(_messageService.Send(anyString(), anyString())).thenReturn(SendingStatus.SENDING_ERROR);
+
+        //Act
+        var result = _target.SendMessage(anyString(), anyString());
+
+        //Assert
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void SendMessage_ForEverythingOk_ShouldInvocationSucceed() throws MalformedRecipientException
+    {
+        //Arrange
+        when(_messageService.Send("Server", "Test")).thenReturn(SendingStatus.SENT);
+
+        //Act
+        var result = _target.SendMessage("Server", "Test");
+
+        //Assert
+        verify(_messageService, atLeastOnce()).Send("Server", "Test");
+    }
+
+    @Test
+    void TestConnection_ForInternalServiceMalfunction_ShouldReturnOne()
+    {
+        //Arrange
+        when(_messageService.CheckConnection(anyString())).thenReturn(ConnectionStatus.FAILURE);
+
+        //Act
+        var result = _target.TestConnection(anyString());
+
+        //Assert
+        Assertions.assertTrue(result == 1);
     }
 }
